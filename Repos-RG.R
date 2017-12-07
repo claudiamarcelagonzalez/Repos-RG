eee949c7f020ac8540caacf606057fdd69c859c7
claudiamarcelagonzalez <cgonzalez@fahce.unlp.edu.ar>
2017-12-07 13:50:49 -0300

    Subo tablas y pruebo FactoMineR

---
+++ Repos-RG.R
@@ -0,0 +1,23 @@
+#setwd("/Users/claudiamgonzalez/Documents/INV/R/Repos-RG/")
+
+# levanto las tablas que me envia Sandra el 30/12/2017
+unlp_ris_rg <- read.csv2("Presencia_investigadores_UNLP_en_RG_y_RIS_parcial.csv", header = TRUE, na.strings = "NA")
+
+# Convierto el data.frame en tipo tibble
+# library(tidyverse)
+# unlp_ris_rg <- as_tibble(unlp_ris_rg) #cambió a tibble..
+
+# Veo de generar el MCA. 
+library(FactoMineR)
+library(ggplot2)
+library(factoextra)
+
+# Selecciono las variables que entrarán - ocho categoricas
+unlp_ris_rg.active <- unlp_ris_rg[1:968, 1:8]
+head(unlp_ris_rg.active[, 1:8], 968)
+summary(unlp_ris_rg.active)
+MCA(unlp_ris_rg.active, ncp = 3, graph = TRUE)
+res.mca <- MCA(unlp_ris_rg.active, graph = FALSE)
+fviz_mca_biplot(res.mca, 
+                repel = TRUE, # Avoid text overlapping (slow if many point)
+                ggtheme = theme_minimal())
