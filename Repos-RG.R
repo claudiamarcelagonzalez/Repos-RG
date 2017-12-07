323567cebd84ac40ea508a6e6ff6abc3bd5a07c3
claudiamarcelagonzalez <cgonzalez@fahce.unlp.edu.ar>
2017-12-07 15:39:18 -0300

    Archivos con nombre de categorias reducido

--- Repos-RG.R
+++ Repos-RG.R
@@ -1,7 +1,7 @@
 #setwd("/Users/claudiamgonzalez/Documents/INV/R/Repos-RG/")
 
 # levanto las tablas que me envia Sandra el 30/12/2017
-unlp_ris_rg <- read.csv2("Presencia_investigadores_UNLP_en_RG_y_RIS_parcial.csv", header = TRUE, na.strings = "NA")
+unlp_ris_rg <- read.csv2("Presencia_investigadores_UNLP_en_RG_y_RIS_parcial1.csv", header = TRUE, na.strings = "NA")
 
 # Convierto el data.frame en tipo tibble
 # library(tidyverse)
@@ -13,8 +13,8 @@ library(ggplot2)
 library(factoextra)
 
 # Selecciono las variables que entrarán - ocho categoricas
-unlp_ris_rg.active <- unlp_ris_rg[1:968, 1:8]
-head(unlp_ris_rg.active[, 1:8], 968)
+unlp_ris_rg.active <- unlp_ris_rg[1:968, 1:6]
+head(unlp_ris_rg.active[, 1:6], 968)
 summary(unlp_ris_rg.active)
 MCA(unlp_ris_rg.active, ncp = 3, graph = TRUE)
 res.mca <- MCA(unlp_ris_rg.active, graph = FALSE)
