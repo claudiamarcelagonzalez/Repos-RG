94ce9c684261a7a7f6c70f9cfcd3a95a3323f858
claudiamarcelagonzalez <cgonzalez@fahce.unlp.edu.ar>
2017-12-07 16:53:24 -0300

    ajustes parametros MCA

--- Repos-RG.R
+++ Repos-RG.R
@@ -1,7 +1,7 @@
 #setwd("/Users/claudiamgonzalez/Documents/INV/R/Repos-RG/")
 
 # levanto las tablas que me envia Sandra el 30/12/2017
-unlp_ris_rg <- read.csv2("Presencia_investigadores_UNLP_en_RG_y_RIS_parcial1.csv", header = TRUE, na.strings = "NA")
+unlp_ris_rg <- read.csv2("Presencia_investigadores_UNLP_en_RG_y_RIS.csv", header = TRUE, na.strings = "NA")
 
 # Convierto el data.frame en tipo tibble
 # library(tidyverse)
@@ -12,12 +12,11 @@ library(FactoMineR)
 library(ggplot2)
 library(factoextra)
 
-# Selecciono las variables que entrarán - ocho categoricas
-unlp_ris_rg.active <- unlp_ris_rg[1:968, 1:6]
-head(unlp_ris_rg.active[, 1:6], 968)
+# Selecciono las variables que entrarán - siete
+unlp_ris_rg.active <- unlp_ris_rg[2:705, c("Categoria", "Facultad", "Sexo", "ResearchGate", "SEDICI", "Memoria", "Naturalis")]
+head(unlp_ris_rg.active[, c("Categoria", "Facultad", "Sexo", "ResearchGate", "SEDICI", "Memoria", "Naturalis")], 706)
 summary(unlp_ris_rg.active)
-MCA(unlp_ris_rg.active, ncp = 3, graph = TRUE)
-res.mca <- MCA(unlp_ris_rg.active, graph = FALSE)
-fviz_mca_biplot(res.mca, 
-                repel = TRUE, # Avoid text overlapping (slow if many point)
-                ggtheme = theme_minimal())
+res.mca <- MCA(unlp_ris_rg.active, ncp = 2, ind.sup = NULL, quanti.sup = NULL, 
+               quali.sup = NULL, excl=NULL, graph = TRUE, 
+               axes = c(1,2), method="Indicator", na.method="NA")
+# fviz_mca_biplot(res.mca, repel = TRUE, ggtheme = theme_minimal())
